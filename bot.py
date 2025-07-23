ec88b82aa930d9feac3bf5cb148ff5790620f14f
aveadikus <abylaiulyadilet@gmail.com>
2025-07-24 02:24:46 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,72 +3,31 @@ import telebot
 TOKEN = "7327734495:AAG9dN0Rb4GtODysq1kRPuofeyDADPieI2I"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    chat_id = message.chat.id
-    bot.send_message(chat_id, 
-        "👋 Сәлем, {0.first_name}!
+    text = f"👋 Сәлем, {message.from_user.first_name}!"           
+    
+    "📚 *Әлемдік YouTube курсы* туралы ақпарат:"     
+    
+    "💼 *Стандарт пакет* — ~20 000₸~ → *10 000₸*"      
+    
+    "✅ Арна ашу
+✅ Номер алу
+✅ Видео монтаждау"          
+"✅ Ниша таңдау
+✅ Әлемге шығу
+(Кері байланыс жоқ)
 
-"
-        "📚 "Әлемдік YouTube курсына" қош келдіңіз!
+"            "👑 *Алтын пакет* — ~100 000₸~ → *50 000₸*
+"            "✅ Барлығы кіреді
+✅ 1 ай кері байланыс
+🎁 Жеке бренд курсы сыйлыққа
 
-"
-        "🔻 Таңдаңыз:
-"
-        "1️⃣ "Стандарт пакет"
-"
-        "2️⃣ "Алтын пакет"".format(message.from_user)
-    )
+"            "💳 *Төлем сілтемесі (Kaspi Pay)*:
+👉 https://pay.kaspi.kz/pay/52bubdrg
 
-@bot.message_handler(func=lambda message: message.text == "1" or "Стандарт" in message.text)
-def standard_package(message):
-    bot.send_message(message.chat.id, 
-        "🎓 "Стандарт пакет"
-"
-        "💸 Бағасы: "20 000₸" ➝ 50% жеңілдікпен "10 000₸"
-
-"
-        "✅ Арна ашу, номер алу, видео монтаж, ниша таңдау, әлемге шығу үйретіледі
-"
-        "⛔ Кері байланыс жоқ
-
-"
-        "💳 Төлем үшін Kaspi👇
-"
-        "https://pay.kaspi.kz/pay/52bubdrg
-"
-        "Каспи пэй сілтеме 🔗"
-    )
-
-@bot.message_handler(func=lambda message: message.text == "2" or "Алтын" in message.text)
-def premium_package(message):
-    bot.send_message(message.chat.id, 
-        "👑 "Алтын пакет"
-"
-        "💸 Бағасы: "100 000₸" ➝ 50% жеңілдікпен "50 000₸"
-
-"
-        "✅ Барлығы кіреді
-"
-        "🧑‍💻 1 ай бойы кері байланыс
-"
-        "🎁 Жеке бренд курсы сыйлыққа!
-
-"
-        "💳 Төлем үшін Kaspi👇
-"
-        "https://pay.kaspi.kz/pay/52bubdrg
-"
-        "Каспи пэй сілтеме 🔗"
-    )
-
-@bot.message_handler(content_types=["photo", "document"])
-def handle_receipt(message):
-    user_link = "https://t.me/aveadikus1"
-    bot.send_message(message.chat.id, 
-        "✅ Чек қабылданды!
-"
-        f"📩 Менеджерге хабарласу: {user_link}"
-    )
+"            "Төлем жасаған соң, чекті маған жіберу үшін осы сілтемеге өтіңіз:
+"            "👉 https://t.me/aveadikus1"
+    bot.send_message(message.chat.id, text, parse_mode='Markdown')
 
 bot.polling()
